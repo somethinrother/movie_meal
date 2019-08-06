ec418182c07ce54ae29a0ab52f22b82b80aae60f
somethinrother <jamesneilson4@gmail.com>
2019-08-06 16:30:31 -0400

    Test for movie title uniqueness

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,13 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
   subject do
-    create(:movie)
+    build(:movie)
   end
 
   describe 'when saving' do
     context 'with valid attributes' do
       it 'saves successfully' do
         expect(subject).to be_valid
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'fails when the name is not unique' do
+        movie = create(:movie, title: subject.title)
+        movie.save
+        expect(subject).to_not be_valid
       end
     end
   end
