2135abb4437b4fca1a09d0e817757c78929cc2f4
somethinrother <jamesneilson4@gmail.com>
2019-08-06 16:07:20 -0400

    Run migration for movies

--- db/migrate/20190806200340_create_movies.rb
+++ db/migrate/20190806200340_create_movies.rb
@@ -1,6 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
+      t.string :name
+      t.string :writers
+			t.integer :year
+		  t.boolean :is_scraped
 
       t.timestamps
     end
