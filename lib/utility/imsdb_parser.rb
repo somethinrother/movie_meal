ba2f445244611fb904cf529999d199506c2988ff
somethinrother <jamesneilson4@gmail.com>
2019-08-08 17:45:48 -0400

    More small refactors

--- lib/utility/imsdb_parser.rb
+++ lib/utility/imsdb_parser.rb
@@ -1,7 +1,8 @@
 module Utility
   class ImsdbParser
     ALL_SCRIPT_URL = 'https://www.imsdb.com/all%20scripts/'.freeze
-    SCRIPT_LINK_CSS_PATH = 'table td p a'.freeze
+    SCRIPT_LINK_CSS_PATH='table td p a'.freeze
+    LANDING_PAGE_CSS_PATH='.script-details td a'.freeze
 
     def all_movie_page_links
       extract_tags_from_css_at_url(SCRIPT_LINK_CSS_PATH, ALL_SCRIPT_URL)
@@ -10,14 +11,14 @@ module Utility
     def extract_script_url_from_node(node)
       attributes = node.attributes
       landing_page_href = "https://www.imsdb.com#{attributes['href'].value}".gsub(' ', '%20')
-      links = extract_tags_from_css_at_url('.script-details td a', landing_page_href)
+      links = extract_tags_from_css_at_url(LANDING_PAGE_CSS_PATH, landing_page_href)
       script_href = links[-1].attributes['href'].value
       "https://www.imsdb.com#{script_href}"
     end
 
     def extract_tags_from_css_at_url(css_path, url)
-      html = HTTParty.get(url).body
-      body = Nokogiri::HTML(html)
+      raw_body = HTTParty.get(url).body
+      body = Nokogiri::HTML(raw_body)
       body.css(css_path)
     end
   end
