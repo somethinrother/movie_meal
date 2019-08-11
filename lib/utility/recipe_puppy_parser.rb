b34e97d8807a473ce38763f1075fe2f3b18bb4a3
David Bell <daverichardbell@gmail.com>
2019-08-11 17:19:27 -0400

    having validation issues with the scraper methods

--- lib/utility/recipe_puppy_parser.rb
+++ lib/utility/recipe_puppy_parser.rb
@@ -3,7 +3,7 @@ module Utility
     ALL_RECIPES_URL = 'http://www.recipepuppy.com/api/?q=&p='.freeze
     ALL_INGREDIENT_RECIPES_URL = 'http://www.recipepuppy.com/api/'.freeze
 
-    def scrape_all_recipes
+    def query_all_recipes
       begin
         recipes = {}
         99.times do |page|
@@ -20,7 +20,7 @@ module Utility
       end
     end
 
-    def scrape_all_recipes_for(*ingredient)
+    def query_all_recipes_for(*ingredient)
       begin
         ing_search = ingredient.join('').gsub(/\s+/, "")
         recipes = {}
@@ -29,60 +29,76 @@ module Utility
           if response.success?
             parsed = JSON.parse(response)
             parsed["results"].each do |recipe|
-              recipes[recipe["title"]] = recipe["ingredients"]
+    	        new_recipe = Recipe.create({ name: recipe["title"], thumbnail: recipe["thumbnail"]}) if recipe
+byebug
+              ingredient_array = recipe["ingredients"].split(', ')
+              ingredient_array.each do |ingredient|
+                new_ingredient = Ingredient.create({ name: ingredient })
+                new_recipe.ingredients.push(new_ingredient) if new_ingredient
+              end
             end
           end
         end
         return recipes
       rescue JSON::ParserError
         puts "JSON::ParserError // API call failed"
-        return recipes
+      rescue ActiveRecord::RecordInvalid
+        puts "Validation failed"
       end
     end
 
-    # def get_one_recipe(*ingredient)
-    #   begin
-    #     ing_search = ingredient.join('').gsub(/\s+/, "")
-    #     response = HTTParty.get("#{ALL_INGREDIENT_RECIPES_URL}?i=#{ing_search}&p=1")
-    #     if response.success?
-    #       parsed = JSON.parse(response)
-    #       recipe = response[parsed["results"].first["title"]] = parsed["results"].first["ingredients"]
-    #     return recipe
-    #   end
-    #   rescue JSON::ParserError
-    #     puts "JSON::ParserError // API call failed"
-    #   rescue ActiveRecord::RecordInvalid
-    #     puts "Validation failed"
-    #   end
-    # end
-
-  def save_all_recipes_to_database
-    begin
-      100.times do |page|
-        response = HTTParty.get("#{ALL_INGREDIENT_RECIPES_URL}?q=&p=#{page + 1}")
+    def get_first_recipe(*ingredient)
+      begin
+        recipe = {}
+        ing_search = ingredient.join('').gsub(/\s+/, "")
+        response = HTTParty.get("#{ALL_INGREDIENT_RECIPES_URL}?i=#{ing_search}&p=1")
         if response.success?
           parsed = JSON.parse(response)
-          parsed["results"].each do |recipe|
-  	        new_recipe = Recipe.create({ name: recipe["title"], thumbnail: recipe["thumbnail"]}) if recipe
-            puts "Extracting from #{page + 1}"
+          recipe = parsed["results"].first
+            new_recipe = Recipe.create({ name: recipe["title"], thumbnail: recipe["thumbnail"]}) if recipe
             ingredient_array = recipe["ingredients"].split(', ')
-            ingredient_array.each do |ingredient|
-              new_ingredient = Ingredient.create({ name: ingredient })
-              new_recipe.ingredients.push(new_ingredient) if new_ingredient
+              ingredient_array.each do |ingredient|
+                new_ingredient = Ingredient.create({ name: ingredient })
+                new_recipe.ingredients.push(new_ingredient) if new_ingredient
+              end
+        return recipe
+      end
+      rescue JSON::ParserError
+        puts "JSON::ParserError // API call failed"
+      rescue ActiveRecord::RecordInvalid
+        puts "Validation failed"
+      end
+    end
+
+    def save_all_recipes_to_database
+      begin
+        100.times do |page|
+          response = HTTParty.get("#{ALL_INGREDIENT_RECIPES_URL}?q=&p=#{page + 1}")
+          if response.success?
+            parsed = JSON.parse(response)
+            parsed["results"].each do |recipe|
+    	        new_recipe = Recipe.create({ name: recipe["title"], thumbnail: recipe["thumbnail"]}) if recipe
+              ingredient_array = recipe["ingredients"].split(', ')
+              ingredient_array.each do |ingredient|
+                new_ingredient = Ingredient.create({ name: ingredient })
+                new_recipe.ingredients.push(new_ingredient) if new_ingredient
+              end
             end
           end
         end
       end
+      rescue JSON::ParserError
+        puts "JSON::ParserError // API call failed"
+      rescue ActiveRecord::RecordInvalid
+        puts "Validation failed"
     end
-    rescue JSON::ParserError
-      puts "JSON::ParserError // API call failed"
-    rescue ActiveRecord::RecordInvalid
-      puts "Validation failed"
-  end
+
+
 
   end
 end
 
+# TODO: Clean up recipe titles with regex, get rid of all \n\t\r
 # Recipe.recipe_parser.save_all_recipes_to_database
-# Recipe.recipe_parser.scrape_all_recipes
-# Recipe.recipe_parser.scrape_all_recipes_for("garlic, onions")
+# Recipe.recipe_parser.get_first_recipe("garlic")
+# Recipe.recipe_parser.query_all_recipes_for("garlic, onions")
