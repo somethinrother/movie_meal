710595a5a27f1bfa458e0eac1b7a8bd91673fbf3
David Bell <daverichardbell@gmail.com>
2019-08-13 14:12:47 -0400

    refactored recipes a lot, everything works, ran database multiple times, doesnt double add anything. i hope you feel good about your tutalage (sp?)- love dave

--- lib/utility/recipe_puppy_parser.rb
+++ lib/utility/recipe_puppy_parser.rb
@@ -8,7 +8,7 @@ module Utility
       ingredient_query_syntax = ingredients.join('').gsub(/\s+/, "")
       contact_recipe_api(ingredient_query_syntax)
     end
-# CHILD 1
+
     def contact_recipe_api(ingredient_query)
       recipes = []
       100.times do |page|
@@ -17,24 +17,26 @@ module Utility
       end
       return recipes
     end
-# CHILD 1
-    def get_recipes(recipe_array, url)
+
+    def get_recipes_to_query(recipe_array, url)
       response = HTTParty.get(url)
-      if response.success?
+      return error unless response.success?
         parsed = JSON.parse(response)
         parsed["results"].each do |recipe|
-          searched_recipe = Recipe.find_by(name: recipe["title"])
-           if searched_recipe
-            recipe_array << searched_recipe
-          else
-            recipe_array << Recipe.create(name: recipe["title"])
-          end
-        end
+          organise_new_vs_old_recipes(recipe)
       end
       return recipe_array
     end
 
-# Complete Scrape PARENT 2
+    def organise_new_vs_old_recipes(results_recipe)
+      recipe_ingredients = results_recipe["ingredients"].split(', ')
+      check_recipes = Recipe.find_by(name: results_recipe["title"])
+        process_ingredients_for(check_recipes, recipe_ingredients) if check_recipes
+      new_recipe = Recipe.create({name: results_recipe["title"], thumbnail: results_recipe["thumbnail"]})
+        process_ingredients_for(new_recipe, recipe_ingredients) if new_recipe
+    end
+
+# Complete Scrape PARENT
     def save_all_recipes_to_database
       recipes = []
   		100.times do |page|
@@ -42,22 +44,16 @@ module Utility
         get_recipes_to_save(recipes, url)
     	end
     end
-# CHILD 2 I could probably refactor this a bit more
+
     def get_recipes_to_save(recipe_array, url)
       response = HTTParty.get(url)
-      if response.success?
-        parsed = JSON.parse(response)
-        parsed["results"].each do |recipe|
-          recipe_ingredients = recipe["ingredients"].split(', ')
-          check_recipes = Recipe.find_by(name: recipe["title"])
-          process_ingredients_for(check_recipes, recipe_ingredients) if check_recipes
-          next if check_recipes
-          new_recipe = Recipe.create({name: recipe["title"], thumbnail: recipe["thumbnail"]})
-          process_ingredients_for(new_recipe, recipe_ingredients) if new_recipe
-          end
+      return error unless response.success?
+      parsed = JSON.parse(response)
+      parsed["results"].each do |recipe|
+        organise_new_vs_old_recipes(recipe)
       end
     end
-# CHILD 2
+
     def process_ingredients_for(recipe, ingredients)
       ingredients.each do |ingredient|
         if Ingredient.find_by(name: ingredient ) && !(recipe.ingredients.find_by(name: ingredient ))
