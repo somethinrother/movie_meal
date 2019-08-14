30d8daaa0f4b9d51dac8178b3445308ae9949f5f
David Bell <daverichardbell@gmail.com>
2019-08-14 10:57:36 -0400

    fixed james issues v1

--- lib/utility/recipe_puppy_parser.rb
+++ lib/utility/recipe_puppy_parser.rb
@@ -3,7 +3,6 @@ module Utility
     ALL_RECIPES_URL = 'http://www.recipepuppy.com/api/?q=&p='.freeze
     BASE_URL = 'http://www.recipepuppy.com/api/'.freeze
 
-# API/db scrape/validation PARENT 1
     def query_all_recipes_for(*ingredients)
       ingredient_query_syntax = ingredients.join('').gsub(/\s+/, "")
       contact_recipe_api(ingredient_query_syntax)
@@ -13,47 +12,34 @@ module Utility
       recipes = []
       100.times do |page|
         searched_url = "#{BASE_URL}?i=#{ingredient_query}&p=#{page + 1}"
-        get_recipes(recipes, searched_url)
+        get_recipes_to_query(recipes, searched_url)
       end
-      return recipes
+      recipes
     end
 
     def get_recipes_to_query(recipe_array, url)
-      response = HTTParty.get(url)
-      return error unless response.success?
-        parsed = JSON.parse(response)
-        parsed["results"].each do |recipe|
-          organise_new_vs_old_recipes(recipe)
-      end
-      recipe_array
-    end
-
-    def organise_new_vs_old_recipes(results_recipe)
-      recipe_ingredients = results_recipe["ingredients"].split(', ')
-      # FIX, dont want to get rid of whitespace
-      recipe_name_no_special_characters = results_recipe["title"].gsub!(/[^0-9A-Za-z]/, '')
-      check_recipes = Recipe.find_by(name: recipe_name_no_special_characters)
-        process_ingredients_for(check_recipes, recipe_ingredients) if check_recipes
-      new_recipe = Recipe.create({name: recipe_name_no_special_characters, thumbnail: results_recipe["thumbnail"]})
-        process_ingredients_for(new_recipe, recipe_ingredients) if new_recipe
-    end
-
-# Complete Scrape PARENT
-    def save_all_recipes_to_database
-      recipes = []
-      100.times do |page|
-        url = "#{ALL_RECIPES_URL}#{page + 1}"
-        get_recipes_to_save(recipes, url)
-    	end
-    end
-
-    def get_recipes_to_save(recipe_array, url)
       response = HTTParty.get(url)
       return error unless response.success?
       parsed = JSON.parse(response)
       parsed["results"].each do |recipe|
         organise_new_vs_old_recipes(recipe)
       end
+      recipe_array
+    end
+
+    def organise_new_vs_old_recipes(results_recipe)
+      ingredients = results_recipe["ingredients"].split(', ')
+      # Double check REGEX is working
+      recipe_name = results_recipe["title"]
+      recipe_name.gsub!(/&quot/, '').gsub!(/[^0-9A-Za-z]/, ' ')
+      check_recipes = Recipe.find_by(name: recipe_name)
+        process_ingredients_for(check_recipes, ingredients) if check_recipes
+
+      new_recipe = Recipe.create({
+        name: recipe_name,
+        thumbnail: results_recipe["thumbnail"]
+        })
+        process_ingredients_for(new_recipe, ingredients) if new_recipe
     end
 
     def process_ingredients_for(recipe, ingredients)
@@ -64,6 +50,23 @@ module Utility
           new_ingredient = Ingredient.create({ name: ingredient })
           recipe.ingredients << new_ingredient
         end
+      end
+    end
+
+    def save_all_recipes_to_database
+      100.times do |page|
+        url = "#{ALL_RECIPES_URL}#{page + 1}"
+        get_recipes_to_save(recipes, url)
+    	end
+    end
+
+    def get_recipes_to_save(recipe_array, url)
+      response = HTTParty.get(url)
+      return error unless response.success?
+      
+      parsed = JSON.parse(response)
+      parsed["results"].each do |recipe|
+        organise_new_vs_old_recipes(recipe)
       end
     end
 
