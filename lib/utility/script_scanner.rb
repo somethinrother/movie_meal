ba27a1c67617cb78fa1780f0f1d9c4f922c45f3f
David Bell <daverichardbell@gmail.com>
2019-08-16 17:22:59 -0400

    trying to find way to display information better, refactoring scanner methods

--- lib/utility/script_scanner.rb
+++ lib/utility/script_scanner.rb
@@ -18,7 +18,6 @@ module Utility
 		end
 
 		def	scan_script(movie)
-			movie = Movie.find_by(title: movie)
 			return NoMovieScript if movie.script.nil?
 
 			words = movie.script.split(' ')
@@ -27,17 +26,60 @@ module Utility
 
 				movie.recipes << Recipe.find_by(name: word) if !(Recipe.find_by(name: word).nil?)
 			end
+			if movie.ingredients.first
+				movie.ingredients.each do |ingredient|
+					ingredient
+				end
+			end
+			if movie.recipes.first
+				movie.recipes.each do |recipe|
+					recipe
+				end
+			end
 		end
 
-# For error finding
+# METHODS FOR FINDING ERRORS AND INFORMATION
 		def check_for_all_scripts
 			movies = Movie.all
 			movies.each do |movie|
 				puts "#{movie.title} has a script" if movie.script
-				puts "#{movie.title} ... NO script" if movie.script.nil?
+				puts "#{movie.title} ... NO script" if movie.script == nil?
 			end
 		end
 
+# need to fix the out put of these methods so its not array return
+# 	movie = Movie.find_by(title: movie)
+
+# need to fix the out put of these methods so its not array return
+		def check_for_food_loop(number)
+			counter = 0
+			Movie.all.each do |movie|
+				if movie.is_scraped && counter < number
+					scan_script(movie)
+					if has_any_food(movie)
+						counter += 1
+						check_for_ingredient_mentions(movie)
+					end
+				end
+			end
+		end
+
+		def has_any_food(movie)
+			return true	if movie.ingredients.all || movie.recipes.all
+		end
+
+		def check_for_ingredient_mentions(movie)
+			if has_any_food(movie)
+				ingredients = []
+				movie.ingredients.all.each do |ingredient|
+					ingredients << ingredient.name unless ingredients.include?(ingredient)
+				end
+				movie.recipes.all.each do |recipe|
+					ingredients << recipe.name
+				end
+				return ingredients
+			end
+		end
 
 	end
 end
