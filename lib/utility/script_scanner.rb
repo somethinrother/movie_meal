507be7656edf3a059b36f5dc2526c4ee09deca22
David Bell <daverichardbell@gmail.com>
2019-08-16 19:38:33 -0400

    script scanner can recognise recipes and ingredients

--- lib/utility/script_scanner.rb
+++ lib/utility/script_scanner.rb
@@ -10,7 +10,6 @@ module Utility
 		end
 
 		def get_script(movie)
-		  movie = Movie.find_by(title: movie)
 			return NoMovieExists unless movie
 
 			parser = Utility::ImsdbParser.new
@@ -18,31 +17,35 @@ module Utility
 		end
 
 		def	scan_script(movie)
-			return NoMovieScript if movie.script.nil?
+			get_script(movie) if movie.is_scraped == false
 
 			words = movie.script.split(' ')
 			words.each do |word|
 				movie.ingredients << Ingredient.find_by(name: word) unless Ingredient.find_by(name: word).nil?
-
-				movie.recipes << Recipe.find_by(name: word) if !(Recipe.find_by(name: word).nil?)
+				# commented out for speed in testing
+				# movie.recipes << Recipe.find_by(name: word) unless Recipe.find_by(name: word).nil?
 			end
 		end
 
 # can alter return data type, right now is an array for proof of concept
-		def movies_check_for_food(number)
+		def check_all_for_food(number)
 			counter = 0
-			Movie.all.each do |movie|
-				if movie.is_scraped && has_any_food(movie) && counter < number
-						scan_script(movie)
-						# display_recipes(movie)
+			if counter < number
+				Movie.all.each do |movie|
+					if movie.is_scraped && movie.ingredients.all
 						counter += 1
+						scan_script(movie)
 						display_ingredients(movie)
+					end
 				end
 			end
 		end
 
-		def has_any_food(movie)
-			movie.ingredients.all || movie.recipes.all
+		def display_food(movie_title)
+			movie = Movie.find_by(title: movie_title)
+			scan_script(movie)
+			display_recipes(movie)
+			display_ingredients(movie)
 		end
 
 		def display_recipes(movie)
@@ -61,7 +64,6 @@ module Utility
 			puts "#{movie.title} contains: #{unique_ingredients.to_s}"
 		end
 
-# METHODS FOR FINDING ERRORS AND INFORMATION
 		def display_all_movies_with_script
 			movies = Movie.all
 			movies.each do |movie|
@@ -73,14 +75,6 @@ module Utility
 			puts "#{movie.title} has a script"	if movie.script
 			puts "#{movie.title} does not have a script" if movie.script == nil?
 		end
-
-		# def all_movies_display_all_food
-		# 	Movie.all.each do |movie|
-		# 		if has_any_food(movie)
-		# 			display_ingredients(movie)
-		# 		end
-		# 	end
-		# end
 
 	end
 end
