8db61b62bd49a845a61f7212a7567a60be435a20
David Bell <daverichardbell@gmail.com>
2019-08-16 12:12:43 -0400

    scriptscanner coming together, havent found a movie with food mentions yet

--- lib/utility/script_scanner.rb
+++ lib/utility/script_scanner.rb
@@ -8,16 +8,14 @@ module Utility
 			return NoMovieExists unless movie_found
 
 			parser = Utility::ImsdbParser.new
-			parser.populate_script(movie_found) if !(movie_found.script.nil?)
-			
+			parser.populate_script(movie_found) if movie_found.script.nil?
 			scan_for_recipes_and_ingredients(movie_found)
 		end
 
 		def	scan_for_recipes_and_ingredients(movie)
-			words = movie[:script].split(' ')
+			words = movie.script.split(' ')
 			words.each do |word|
-				byebug
-				movie.ingredients << Ingredient.find_by(name: word) if (Ingredient.find_by(name: word).nil?)
+				movie.ingredients << Ingredient.find_by(name: word) unless Ingredient.find_by(name: word).nil?
 
 				movie.recipes << Recipe.find_by(name: word) if !(Recipe.find_by(name: word).nil?)
 			end
