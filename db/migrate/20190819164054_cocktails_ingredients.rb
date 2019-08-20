6bfe3a1c7697d38540fb0f78efcc74b57b223e32
David Bell <daverichardbell@gmail.com>
2019-08-20 09:56:21 -0400

    cleaning up cocktail stuff

--- db/migrate/20190819164054_cocktails_ingredients.rb
+++
@@ -1,8 +0,0 @@
-class CocktailsIngredients < ActiveRecord::Migration[5.2]
-  def change
-    create_table :cocktails_ingredients, id: false do |t|
-      t.belongs_to :cocktails, index: true
-      t.belongs_to :ingredients, index: true
-    end
-  end
-end
