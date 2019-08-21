d142fc6cee718342c767395ab1b56081778f7799
David Bell <daverichardbell@gmail.com>
2019-08-21 11:07:54 -0400

    deleted duplicate utility.new call in model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,8 +4,4 @@ class Recipe < ApplicationRecord
 	validates :name, uniqueness: true
 	has_and_belongs_to_many :movies
 	has_and_belongs_to_many :ingredients
-	
-	def self.recipe_parser
-		Utility::RecipePuppyParser.new
-	end
 end
