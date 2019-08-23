22999ba3b422ac4ec61ea8720df0126f3e37d56a
somethinrother <jamesneilson4@gmail.com>
2019-08-22 21:28:14 -0400

    Button works as expected!

--- app/javascript/components/IngredientDisplay.js
+++ app/javascript/components/IngredientDisplay.js
@@ -1,13 +1,33 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+const GET_INGREDIENTS_REQUEST = 'GET_INGREDIENTS_REQUEST'
+
+function getIngredients() {
+  console.log('getIngredients() Action!!');
+  return {
+    type: GET_INGREDIENTS_REQUEST
+  }
+}
+
 class IngredientDisplay extends React.Component {
   render () {
     return (
       <React.Fragment>
         <div>Hi there</div>
+
+        <button className="button" onClick={() => this.props.getIngredients()}>Get Ingredients</button>
       </React.Fragment>
     );
   }
 }
 
-export default IngredientDisplay
+const structuredSelector = createStructuredSelector({
+  ingredients: state => state.ingredients
+});
+
+const mapDispatchToProps = { getIngredients };
+
+export default connect(structuredSelector, mapDispatchToProps)(IngredientDisplay);
