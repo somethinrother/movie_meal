4fb73a92bc240b1d423615f3454f94b153f5e005
somethinrother <jamesneilson4@gmail.com>
2019-08-22 20:56:59 -0400

    Get root rendering react

---
+++ app/javascript/components/IngredientDisplay.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class IngredientDisplay extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div>Hi there</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default IngredientDisplay
