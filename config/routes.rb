54269c5ade7370f30bde18410659d0add907ab91
somethinrother <jamesneilson4@gmail.com>
2019-09-10 18:51:07 -0400

    More stuff removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-
-  require 'sidekiq/web'
-  mount Sidekiq::Web => "/sidekiq"
-
   namespace :v1, defaults: { format: 'json' } do
     resources :ingredients
   end
