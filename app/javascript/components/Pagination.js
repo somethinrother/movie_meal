68945ffab74877c7aca74cd8dfec0ea9636e8ec9
David Bell <daverichardbell@gmail.com>
2019-10-03 23:04:19 -0400

    fixed pagination issue temporarily, by making a links buttons, though need to fix URL issue with a links still, or style with css to hide buttons

--- app/javascript/components/Pagination.js
+++ app/javascript/components/Pagination.js
@@ -1,18 +1,20 @@
 import React from "react";
 
-const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+const Pagination = ({ moviesPerPage, totalPosts, paginate }) => {
   const pageNumbers = [];
-  console.log(pageNumbers);
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+
+  for (let i = 1; i <= Math.ceil(totalPosts / moviesPerPage); i++) {
     pageNumbers.push(i);
   }
 
   return (
-    <nav>
+    <span>
       {pageNumbers.map(number => (
-        <li>{number}</li>
+        <button key={number} onClick={() => paginate(number)}>
+          {number}
+        </button>
       ))}
-    </nav>
+    </span>
   );
 };
 
