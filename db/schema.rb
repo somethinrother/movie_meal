47d526920cc8b237eb631f4ccaf25873abf4c0a6
David Bell <daverichardbell@gmail.com>
2019-10-11 21:08:26 -0400

    added uniqueness validation for movies_recipes_associations with an add_index for migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_214025) do
+ActiveRecord::Schema.define(version: 2019_10_12_010442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2019_10_09_214025) do
     t.decimal "mentions_percentage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["movie_id", "recipe_id"], name: "index_movies_recipes_associations_on_movie_id_and_recipe_id", unique: true
     t.index ["movie_id"], name: "index_movies_recipes_associations_on_movie_id"
     t.index ["recipe_id"], name: "index_movies_recipes_associations_on_recipe_id"
   end
