b8a8aa1680dd1be0d3d491ef2bcfe4f95f04ba9e
David Bell <daverichardbell@gmail.com>
2019-10-21 17:14:02 -0400

    pagination display not crashing, looks better, need to add First, last, <<, >> functionality

--- app/javascript/components/Pagination.js
+++ app/javascript/components/Pagination.js
@@ -3,14 +3,38 @@ import "./MovieContainer.css";
 import _ from "lodash";
 
 const Pagination = ({ totalPosts, moviesPerPage, paginate, currentPage }) => {
-  console.log(currentPage);
-  const pagesCount = Math.ceil(totalPosts / moviesPerPage);
-  if (pagesCount === 1) return null;
-  const pages = _.range(1, pagesCount + 1);
+  let startPage = 0;
+  let endPage = 0;
+  let pageSize = 10;
+
+  const totalPages = Math.ceil(totalPosts / moviesPerPage);
+  if (totalPages === 1) return null;
+  const all_pages = _.range(1, totalPages + 1);
+
+  if (currentPage <= 6) {
+    startPage = 1;
+    endPage = 10;
+  } else if (currentPage + 4 >= totalPages) {
+    startPage = totalPages - 9;
+    endPage = totalPages;
+  } else {
+    startPage = currentPage - 5;
+    endPage = currentPage + 4;
+  }
+
+  // // calculate start and end item indexes
+  let startIndex = (currentPage - 1) * pageSize;
+  let endIndex = Math.min(startIndex + pageSize - 1, totalPosts - 1);
+
+  // create an array of pages to ng-repeat in the pager control
+  let pages = [...Array(endPage + 1 - startPage).keys()].map(
+    i => startPage + i
+  );
 
   return (
     <nav>
       <ul className="pagination">
+        <li className="page-item">First</li>
         <li className="page-item">&lt;&lt;</li>
         {pages.map(page => (
           <li
@@ -23,6 +47,7 @@ const Pagination = ({ totalPosts, moviesPerPage, paginate, currentPage }) => {
           </li>
         ))}
         <li className="page-item">&gt;&gt;</li>
+        <li className="page-item">Last</li>
       </ul>
     </nav>
   );
