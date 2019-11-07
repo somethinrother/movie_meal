669c1f06cfab062f84d0b3fe196e67fe4ac49847
somethinrother <jamesneilson4@gmail.com>
2019-11-07 15:38:02 -0500

    Extract controller methods into concern

--- app/controllers/v1/movies_controller.rb
+++ app/controllers/v1/movies_controller.rb
@@ -1,4 +1,6 @@
 class V1::MoviesController < ApplicationController
+  include MoviesDisplayMethods
+
   def index
     movies = Movie.all
     movies.reject { |movie| movie.script.nil? }
@@ -10,47 +12,6 @@ class V1::MoviesController < ApplicationController
 
   def show
     movie = Movie.find(params[:id])
-
-    render json: {
-      movie: movie,
-      ingredient_map: find_or_populate_movie_ingredients(movie),
-      recipe_map: find_or_populate_movie_recipes(movie)
-    }
-  end
-
-  private
-
-  def find_or_populate_movie_ingredients(movie)
-    movie_ingredients = MoviesIngredientsAssociation.where(movie: movie)
-
-    if movie_ingredients.empty?
-      ingredient_ranker = Utility::IngredientParser.new(movie)
-      ingredient_ranker.create_movie_ingredients_associations
-      movie_ingredients = MoviesIngredientsAssociation.where(movie: movie)
-    end
-
-    prepare_movie_ingredients_for_display(movie_ingredients)
-  end
-
-  def prepare_movie_ingredients_for_display(movie_ingredients)
-    sorted_movie_ingredients = movie_ingredients.sort_by(&:mentions).reverse
-    sorted_movie_ingredients.map { |association| [association, association.ingredient.name] }
-  end
-
-  def find_or_populate_movie_recipes(movie)
-    movie_recipes = MoviesRecipesAssociation.where(movie: movie)
-
-    if movie_recipes.empty?
-      recipe_ranker = Utility::RecipeRanker.new(movie)
-      recipe_ranker.create_movie_recipes_associations
-      movie_recipes = MoviesRecipesAssociation.where(movie: movie)
-    end
-
-    prepare_movie_recipes_for_display(movie_recipes)
-  end
-
-  def prepare_movie_recipes_for_display(movie_recipes)
-    sorted_movie_recipes = movie_recipes.sort_by(&:mentions).reverse
-    sorted_movie_recipes.map { |association| [association, association.recipe.name] }
+    render json: prepare_json_response(movie)
   end
 end
