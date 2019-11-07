dd67b3bba0240a0dbe89f2f8964b1cadbfe690e4
somethinrother <jamesneilson4@gmail.com>
2019-11-07 15:26:37 -0500

    Begin refactor in movies controller

--- app/controllers/v1/movies_controller.rb
+++ app/controllers/v1/movies_controller.rb
@@ -1,7 +1,7 @@
 class V1::MoviesController < ApplicationController
   def index
     movies = Movie.all
-    movies.select {|movie| !movie.script.nil? }
+    movies.reject { |movie| movie.script.nil? }
 
     render json: {
       movies: movies
@@ -10,47 +10,47 @@ class V1::MoviesController < ApplicationController
 
   def show
     movie = Movie.find(params[:id])
-    movie_ingredients = movie.ingredients
-    movie_ingredients_associations = find_or_populate_movie_ingredients_associations(movie)
 
-    if movie_ingredients.empty?
-      script_scanner = Utility::ScriptScanner.new
-      script_scanner.get_ingredients_from_script(movie)
-    end
-    
-    # sort ingredient mentions
-    sorted_movie_ingredients = movie_ingredients_associations.sort_by {|association| association.mentions}.reverse
-    shape_movie_ingredients_data = sorted_movie_ingredients.map{ |association| [association, association.ingredient.name] }
-
-    # select recipe mentions
-    movie_recipes = MoviesRecipesAssociation.all.select { |association| association.movie === movie }
-    if movie_recipes.empty?
-      recipe_ranker = Utility::RecipeRanker.new(movie)
-      recipe_ranker.create_movie_recipes_associations
-    end
-    movie_recipes = MoviesRecipesAssociation.all.select { |association| association.movie === movie }
-
-    # sort for output
-    sorted_movie_recipes = movie_recipes.sort_by {|recipe_association| recipe_association.mentions.length}.reverse
-    shape_movie_recipes_data = sorted_movie_recipes.map{ |association| [association, association.recipe.name] }
-    
     render json: {
       movie: movie,
-      ingredient_map: shape_movie_ingredients_data,
-      recipe_map: shape_movie_recipes_data
+      ingredient_map: find_or_populate_movie_ingredients(movie),
+      recipe_map: find_or_populate_movie_recipes(movie)
     }
   end
 
   private
 
-  def find_or_populate_movie_ingredients_associations(movie)
-    movie_associations = MoviesIngredientsAssociation.where(movie: movie)
+  def find_or_populate_movie_ingredients(movie)
+    movie_ingredients = MoviesIngredientsAssociation.where(movie: movie)
 
-    if movie_associations.empty?
+    if movie_ingredients.empty?
       ingredient_ranker = Utility::IngredientParser.new(movie)
       ingredient_ranker.create_movie_ingredients_associations
+      movie_ingredients = MoviesIngredientsAssociation.where(movie: movie)
     end
 
-    MoviesIngredientsAssociation.where(movie: movie)
+    prepare_movie_ingredients_for_display(movie_ingredients)
+  end
+
+  def prepare_movie_ingredients_for_display(movie_ingredients)
+    sorted_movie_ingredients = movie_ingredients.sort_by(&:mentions).reverse
+    sorted_movie_ingredients.map { |association| [association, association.ingredient.name] }
+  end
+
+  def find_or_populate_movie_recipes(movie)
+    movie_recipes = MoviesRecipesAssociation.where(movie: movie)
+
+    if movie_recipes.empty?
+      recipe_ranker = Utility::RecipeRanker.new(movie)
+      recipe_ranker.create_movie_recipes_associations
+      movie_recipes = MoviesRecipesAssociation.where(movie: movie)
+    end
+
+    prepare_movie_recipes_for_display(movie_recipes)
+  end
+
+  def prepare_movie_recipes_for_display(movie_recipes)
+    sorted_movie_recipes = movie_recipes.sort_by(&:mentions).reverse
+    sorted_movie_recipes.map { |association| [association, association.recipe.name] }
   end
 end
