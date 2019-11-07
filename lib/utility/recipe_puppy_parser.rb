426ac6210c8ae93b282fbf074059c65202b89aef
somethinrother <jamesneilson4@gmail.com>
2019-11-07 18:10:09 -0500

    Clean up recipe puppy parser

--- lib/utility/recipe_puppy_parser.rb
+++ lib/utility/recipe_puppy_parser.rb
@@ -1,31 +1,25 @@
+# frozen_string_literal: true
+
 module Utility
   class RecipePuppyParser
-    ALL_RECIPES_URL = 'http://www.recipepuppy.com/api/?q=&p='.freeze
-    RECIPE_QUERY_BASE = 'http://www.recipepuppy.com/api/?q='.freeze
-    MAX_PAGES = 100.freeze
+    ALL_RECIPES_URL = 'http://www.recipepuppy.com/api/?q=&p='
+    RECIPE_QUERY_BASE = 'http://www.recipepuppy.com/api/?q='
+    MAX_PAGES = 100
 
     def save_all_recipes_to_database
       MAX_PAGES.times do |page|
         url = "#{ALL_RECIPES_URL}#{page + 1}"
         query_for_recipes(url)
-    	end
-    end
-
-    def get_recipes_to_query(recipes_array, url)
-      response = HTTParty.get(url)
-      return JSON::ParserError unless response.success?
-
-      parsed = JSON.parse(response)
-      parsed["results"].each do |recipe|
-        create_new_recipe(recipe)
       end
     end
 
+    private
+
     def create_new_recipe(recipe_data)
-      ingredients = recipe_data["ingredients"].split(', ')
-      recipe_name = recipe_data["title"]
+      ingredients = recipe_data['ingredients'].split(', ')
+      recipe_name = recipe_data['title']
       recipe_name.gsub!(/[^0-9A-Za-z]/, ' ')
-      recipe = Recipe.find_or_create_by(name: recipe_name, thumbnail: recipe_data["thumbnail"])
+      recipe = Recipe.find_or_create_by(name: recipe_name, thumbnail: recipe_data['thumbnail'])
       puts "#{recipe.name} created!"
       associate_ingredients_to_recipe(recipe, ingredients) if recipe
     end
@@ -34,7 +28,7 @@ module Utility
       ingredients.each do |ingredient_name|
         ingredient = Ingredient.find_or_create_by(name: ingredient_name)
         current_ingredients = recipe.ingredients
-        current_ingredients << ingredient if !current_ingredients.include?(ingredient)
+        current_ingredients << ingredient unless current_ingredients.include?(ingredient)
       end
     end
 
@@ -43,7 +37,7 @@ module Utility
       return JSON::ParserError unless response.success?
 
       parsed = JSON.parse(response)
-      parsed["results"].each do |recipe|
+      parsed['results'].each do |recipe|
         create_new_recipe(recipe)
       end
     end
